71e68e34d6eed5bf86786de44b5f787196d1c069
bota-rabbit <rabbit_one75@yahoo.co.jp>
2025-12-10 21:18:11 +0900

    Update settings.py

--- donut_order_system/settings.py
+++ donut_order_system/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-o8@x3xvzq07oz)ofqvznzn#dq1351%z8f1c)ds-82be_0d446u'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
