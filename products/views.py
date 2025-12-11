9a018cd70b36e07ca27282fc682d0b41548d0c58
bota-rabbit <rabbit_one75@yahoo.co.jp>
2025-12-11 15:16:37 +0900

    Add product list view and URL routing

--- products/views.py
+++ products/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.views import generic
+from .models import Category, Product
 
-# Create your views here.
+
+class IndexView(generic.ListView):
+    model = Product
+
